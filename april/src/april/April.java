af3c23ab755d62b29234e4ef4e4d2c145e1bc7cb
soldesk <soldesk@soldesk-PC>
2019-03-13 17:21:12 +0900

    카테고리별로 데이터 추출 완성

--- april/src/april/April.java
+++ april/src/april/April.java
@@ -17,26 +17,23 @@ public class April {
 	static String HAIRBODY = BASE_URL + "/product/list.html?cate_no=52";
 	static String MAKEUP = BASE_URL + "/product/list.html?cate_no=55";
 	
-	void category(String link) throws IOException {
+	public void category(String link) throws IOException {
 		Document doc = Jsoup.connect(link).get(); // 카테고리 링크 
 		Elements img = doc.select("div.thumbnail a");	
 		Elements img1 = doc.select("div.thumbnail a img");	
 		Elements count = doc.select("p.prdCount strong");
 		
 		int c = Integer.parseInt(count.text());
+		System.out.println("상품 수 :" + c);
 		
 		for(int i = 0; i<c; i++) {
-			System.out.println();
-			System.out.println("=======================");
-			System.out.println();
-			String str = img.eq(i).attr("href").replaceAll("&display_group=1", "");
-			String imgsrc = img1.eq(i).attr("src");
+			String str = img.eq(i).attr("href").replaceAll("&display_group=1", ""); // 불필요한 링크 삭제
+			String imgsrc = img1.eq(i).attr("src"); // img의 src 속성
 			String detail = BASE_URL + str;
 			System.out.println(detail);	// 제품상세 페이지 주소
 			System.out.println(imgsrc); // 제품이미지 주소
 			Document doc1 = Jsoup.connect(detail).get();
 			Elements title = doc1.select("div.infoArea h2"); 
-			//Elements table = doc1.select("div.table_april td:nth-of-type(1)");
 			System.out.println(title.text()); // 제품명
 			Elements t = doc1.select("div.table_april tr th");
 			String[] arr = new String[t.size()];
@@ -48,6 +45,7 @@ public class April {
 					System.out.println(ingdnt.eq(j).text()); // 전성분
 				}
 			}
+			System.out.println("\n" + "=======================" + "\n");
 		}
 	}
 	
@@ -55,28 +53,16 @@ public class April {
 		
 		April april = new April();
 		
-		System.out.println("======================베이스============================");
+		System.out.println("======================베이스============================" + "\n");
 		april.category(BASE);
-		
-		System.out.println("======================클렌징============================");
+		System.out.println("======================클렌징============================" + "\n");
 		april.category(CLEANSING);
-		
-		System.out.println("======================스킨케어============================" +"\n");
+		System.out.println("======================스킨케어============================" + "\n");
 		april.category(SKIN);
-		System.out.println("======================헤어바디============================" +"\n");
+		System.out.println("======================헤어바디============================" + "\n");
 		april.category(HAIRBODY);
-		System.out.println("======================메이크업============================" +"\n");
+		System.out.println("======================메이크업============================" + "\n");
 		april.category(MAKEUP);
 
-		
-		
-		/*
-		Document doc = Jsoup.connect(BASE).get(); // 베이스 카테고리 링크 
-		Elements img = doc.select("div.thumbnail a");	
-		Elements img1 = doc.select("div.thumbnail a img");	
-		Elements count = doc.select("p.prdCount strong");
-		
-		int c = Integer.parseInt(count.text());
-		*/
 	}
 }
